e4ae1d100bc5125b506a7b2e8b9658163bbaf406
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-04-04 15:36:50 +0200

    update quadrature construction

--- src/utilities/cache_quadrules.jl
+++ src/utilities/cache_quadrules.jl
@@ -1,13 +1,13 @@
 # Cache quadrature rules
-const squarerules = [GaussQuadrature(Dim{2}, RefCube(), order) for order = 1:5]
-const cuberules = [GaussQuadrature(Dim{3}, RefCube(), order) for order = 1:5]
-const trirules = [GaussQuadrature(Dim{2}, RefTetrahedron(), order) for order = 1:5]
+const squarerules = [QuadratureRule(Dim{2}, RefCube(), order) for order = 1:5]
+const cuberules = [QuadratureRule(Dim{3}, RefCube(), order) for order = 1:5]
+const trirules = [QuadratureRule(Dim{2}, RefTetrahedron(), order) for order = 1:5]
 
 function get_gaussrule(::Type{Dim{2}}, ::RefTetrahedron, order::Int)
     if order <= 5
         return trirules[order]
     else
-        return GaussQuadrature(Dim{2}, RefTetrahedron(), order)
+        return QuadratureRule(Dim{2}, RefTetrahedron(), order)
     end
 end
 
@@ -15,7 +15,7 @@ function get_gaussrule(::Type{Dim{2}}, ::RefCube, order::Int)
     if order <= 5
         return squarerules[order]
     else
-        return GaussQuadrature(Dim{2}, RefCube(), order)
+        return QuadratureRule(Dim{2}, RefCube(), order)
     end
 end
 
@@ -23,6 +23,6 @@ function get_gaussrule(::Type{Dim{3}}, ::RefCube, order::Int)
     if order <= 5
         return cuberules[order]
     else
-        return GaussQuadrature(Dim{3}, RefCube(), order)
+        return QuadratureRule(Dim{3}, RefCube(), order)
     end
 end
